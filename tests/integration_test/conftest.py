84d5feadf0a58813a750cef263aebde4701d04b3
saminyead <Saminyead@gmail.com>
2024-03-21 14:32:12 +0600

    setting up pytest - test for invalid file formate - test for valid api response

---
+++ tests/integration_test/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from rest_framework.test import APIClient
+from rest_framework import status
+from data_cleansing_server.urls import DATATYPE_ENDPOINT
+from datatype.urls import INFER_ENDPOINT
+
+@pytest.fixture(scope='session')
+def api_client() -> APIClient:
+    return APIClient()
+
+
+@pytest.fixture
+def api_endpoint() -> str:
+    return f"{DATATYPE_ENDPOINT}{INFER_ENDPOINT}"
---
+++ tests/integration_test/test_api.py
@@ -0,0 +1,51 @@
+import pytest
+from rest_framework.test import APIClient
+from rest_framework.response import Response
+
+
+INVALID_FILE:str = './files/invalid.pdf'
+DATA_FILE:str = './files/data.csv'
+
+
+@pytest.mark.django_db
+def test_api_invalid_file_format(
+    api_client:APIClient,
+    api_endpoint: str,
+    filename:str = INVALID_FILE
+) -> None:
+    response:Response = api_client.post(
+        path=api_endpoint,
+        data={'file':f'{filename}'},
+        format='multipart'
+    )
+
+    assert response.status_code == 400
+
+
+
+@pytest.mark.django_db
+def test_api_data_file_format(
+    api_client:APIClient,
+    api_endpoint: str,
+    filename:str = DATA_FILE
+) -> None:
+    response:Response = api_client.post(
+        path=api_endpoint,
+        data={'file':f'{filename}'},
+        format='multipart'
+    )
+
+    expected_values:dict= {
+        'Birthday':'datetime64[ns]',
+        'Test_None_Datetime':'datetime64[ns]',
+        'Score':'float64',
+        'Test_None_Numeric':'float64',
+        'Test_All_Int':'int64',
+    }
+
+    assert response.status_code == 200
+    
+    for value in expected_values:
+        assert value in response.data
+
+    
