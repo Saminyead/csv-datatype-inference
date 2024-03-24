c6f4688c64d8c44d47e2517918d1b48d6bc9f965
saminyead <Saminyead@gmail.com>
2024-03-24 14:10:21 +0600

    test endpoint removed

--- datatype/urls.py
+++ datatype/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import view_user_detail,csv_file_upload
+from .views import csv_file_upload
 
 from django.urls.resolvers import URLPattern
 
 INFER_ENDPOINT:str = ''
 
 urlpatterns: list[URLPattern] = [
-    path('user_detail/',view_user_detail,name="user_detail"),
     path(INFER_ENDPOINT,view=csv_file_upload,name='file_upload')
 ]
