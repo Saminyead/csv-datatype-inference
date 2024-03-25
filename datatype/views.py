010cb927ccb42fe82df48b9f02108d80d1c87b9c
saminyead <Saminyead@gmail.com>
2024-03-25 13:45:47 +0600

    refactor - took code from being nested in serializer.is_valid() if block

--- datatype/views.py
+++ datatype/views.py
@@ -12,7 +12,6 @@ from .serializers import FileUploadSerializer
 from .utils.infer_all import infer_all
 
 
-# not yet implemented
 def _check_csv(file:UploadedFile) -> bool|Response:
     """Check if uploaded file is CSV"""
     file_name_split:list[str] = file.name.split('.')
@@ -31,26 +30,27 @@ def csv_file_upload(
     if request.method=='POST':
         serializer = FileUploadSerializer(data=request.data)
         
-        if serializer.is_valid():
-            file:UploadedFile = serializer.validated_data['file']
-            
-            if not _check_csv(file):
-                return Response(
-                    f"The uploaded file {file} is not a CSV file",
-                    status.HTTP_400_BAD_REQUEST
-                )
-
-            data_types = infer_all(file)
-
-            uploaded_file = UploadedCSVFile.objects.create(
-                file=file,
-                original_data_types=data_types['original'],
-                inferred_data_types=data_types['inferred']
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        file:UploadedFile = serializer.validated_data['file']
+        
+        if not _check_csv(file):
+            return Response(
+                f"The uploaded file {file} is not a CSV file",
+                status.HTTP_400_BAD_REQUEST
             )
 
-            response_data = uploaded_file.arrange_for_response()
+        data_types = infer_all(file)
+
+        uploaded_file = UploadedCSVFile.objects.create(
+            file=file,
+            original_data_types=data_types['original'],
+            inferred_data_types=data_types['inferred']
+        )
+
+        response_data = uploaded_file.arrange_for_response()
 
 
-            return Response(response_data,status=status.HTTP_201_CREATED)
+        return Response(response_data,status=status.HTTP_201_CREATED)
         
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
