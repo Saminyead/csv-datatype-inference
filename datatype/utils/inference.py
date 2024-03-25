907900bd55fb9001262015968a60c384d462665b
saminyead <Saminyead@gmail.com>
2024-03-25 12:09:52 +0600

    added infer_datetime() to DataFrameToInfer class

--- datatype/utils/inference.py
+++ datatype/utils/inference.py
@@ -10,7 +10,17 @@ class DataFrameToInfer(pd.DataFrame):
 
 
     def infer_numeric(self) -> "DataFrameToInfer":
-        df = self.copy()      # to prevent changing the original dataframe
+        """Finds which column of the dataframe are of a numeric type. 
+        The criteria for the column to be numeric is - it has
+        to have at least 80% of the entries as numeric.
+
+        Args:
+            df (pd.DataFrame): dataframe whose column's data type is to be inferred
+
+        Returns:
+            pd.DataFrame: the dataframe with the correct data column data type
+        """
+        df: pd.DataFrame = self.copy()      # to prevent changing the original dataframe
         len_df: int = len(df)
 
         for col in df.columns:
@@ -21,9 +31,46 @@ class DataFrameToInfer(pd.DataFrame):
                 df[col] = df_converted
 
         return DataFrameToInfer(df)
+    
+    
+    def _convert_to_datetime(self,col:pd.Series) -> pd.Series:
+        """Helper function to convert column data type to datetime format"""
+        col_dt:pd.Series = col.apply(
+            lambda x: parser.parse(str(x), fuzzy=True).date() \
+                if pd.notnull(x) else pd.NaT # type: ignore
+        )
+        col = col_dt
+        col = pd.to_datetime(col)
+        return col
 
-    def infer_datetime(self):
-        pass
+    
+    def infer_datetime(self) -> "DataFrameToInfer":
+        """
+        Goes through each column of dataframe to check if it contains datetime 
+        values. If it does, then converts the column to datetime format 
+        ('yyyy-mm-dd'), and coverts the column data type to datetime[ns64]
+        
+        Args:
+            df (pandas.DataFrame): The dataframe whose columns are to be 
+                converted.
+            
+        Returns:
+            pandas.DataFrame: The converted column with datetime format 
+                ('yyyy-mm-dd').
+        """
+        df = self.copy()
+        for col in df.columns:
+            if df[col].dtypes!='object':
+                continue
+            try:
+                df[col] = _convert_to_datetime(df[col])
+            except (parser.ParserError, TypeError):
+                continue
+        return DataFrameToInfer(df)
+
+
+
+
 
 def infer_numeric(df:pd.DataFrame) -> pd.DataFrame:
     """Finds which column of the dataframe are of a numeric type. 
