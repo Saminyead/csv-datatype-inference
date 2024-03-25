9bd356b91997550fb66b14888aee22bc300824e9
saminyead <Saminyead@gmail.com>
2024-03-25 12:19:33 +0600

    removed unnecessary imports from inference; class OriginalDataTypes and InferredDataTypes removed (not going to be used)

--- datatype/models.py
+++ datatype/models.py
@@ -1,22 +1,8 @@
 from django.db import models
 
-from .utils.inference import (
-    infer_datetime,infer_numeric
-)
-
 # Create your models here.
 class UploadedCSVFile(models.Model):
     file = models.FileField()
     uploaded_on = models.DateTimeField(auto_now_add=True)
     original_data_types = models.JSONField(null=True,blank=True)
     inferred_data_types = models.JSONField(null=True,blank=True)
-
-# TODO: a model for the original data types
-    # - a model for the converted data types
-
-class OriginalDataTypes(models.Model):
-    pass
-
-
-class InferredDataTypes(models.Model):
-    pass
