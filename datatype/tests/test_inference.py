5206c247a6c303b5158a9487ad9db08ba5a347c4
saminyead <Saminyead@gmail.com>
2024-03-25 13:00:09 +0600

    test cases for infer_category()

--- datatype/tests/test_inference.py
+++ datatype/tests/test_inference.py
@@ -8,39 +8,49 @@ from datatype.utils.infer_all import infer_all
 SAMPLE_DATA_PATH = "sample_data.csv"
 
 @pytest.fixture
-def df_to_infer_test(filename:str=SAMPLE_DATA_PATH) -> DataFrameToInfer:
+def df_test(filename:str=SAMPLE_DATA_PATH) -> pd.DataFrame:
     """Test Dataframe fixture"""
-    df = pd.read_csv(filename)
-    return DataFrameToInfer(df)
+    return pd.read_csv(filename)
 
 
-def test_infer_numeric_80_pct_col_is_numeric(df_to_infer_test:DataFrameToInfer) -> None:
+def test_infer_numeric_80_pct_col_is_numeric(df_test:DataFrameToInfer) -> None:
     """Test checks if infer_numeric categorizes columns with more than 80%
     numeric values as numeric"""
+    df_to_infer_test: DataFrameToInfer = DataFrameToInfer(df_test)
     df_test_inferred: DataFrameToInfer = df_to_infer_test.infer_numeric()
     assert df_test_inferred['Score'].dtype == numpy.dtype('float')
 
 
 
 def test_infer_datetime_varied_dt_formats(
-        df_to_infer_test:DataFrameToInfer
+        df_test:DataFrameToInfer
 ) -> None:
     """Test checks if infer_datetime categorizes columns containing a variety
     of datetime formats"""
+    df_to_infer_test: DataFrameToInfer = DataFrameToInfer(df_test)
     df_test_inferred: DataFrameToInfer = df_to_infer_test.infer_datetime()
     assert df_test_inferred['Birthdate'].dtype == 'datetime64[ns]'
     assert df_test_inferred['Test_None_Datetime'].dtype == 'datetime64[ns]'
 
 
 def test_infer_datetime_no_change_to_non_dt_formats(
-        df_to_infer_test:DataFrameToInfer
+        df_test:DataFrameToInfer
 ) -> None:
     """infer_datetime should not change other formats (especially integer 
     column that are still string/objects) to datetime formats"""
+    df_to_infer_test: DataFrameToInfer = DataFrameToInfer(df_test)
     df_test_inferred: DataFrameToInfer = df_to_infer_test.infer_datetime()
     assert df_test_inferred['Test_None_Numeric'].dtype != 'datetime64[ns]'
     assert df_test_inferred['Test_All_Int'].dtype != 'datetime64[ns]'
     assert df_test_inferred['Score'].dtype != 'datetime64[ns]'
+
+
+def test_infer_category_on_grades_col(
+        df_test:pd.DataFrame
+) -> None:
+    df_to_infer_test: DataFrameToInfer = DataFrameToInfer(df_test)
+    df_test_inferred: DataFrameToInfer = df_to_infer_test.infer_category()
+    assert df_test_inferred['Grade'].dtype == 'category'
 
 
 def test_infer_all_inferred(
@@ -49,12 +59,13 @@ def test_infer_all_inferred(
     df_test_column_dtypes = infer_all(csv_file)
 
     assert df_test_column_dtypes['inferred']['Name'] == 'object'
-    assert df_test_column_dtypes['inferred']['Grade'] == 'object'
+    assert df_test_column_dtypes['inferred']['Grade'] == 'category'
     assert df_test_column_dtypes['inferred']['Remarks'] == 'object'
     assert df_test_column_dtypes['inferred']['Score'] == 'float64'
     assert df_test_column_dtypes['inferred']['Test_None_Numeric'] == 'float64'
     assert df_test_column_dtypes['inferred'][
         'Test_None_Datetime'] == 'datetime64[ns]'
     assert df_test_column_dtypes['inferred']['Birthdate'] == 'datetime64[ns]'
+    assert df_test_column_dtypes['inferred']['Test_None_Datetime'] == 'datetime64[ns]'
 
 
