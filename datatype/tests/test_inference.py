55815bd90d69491ad9dd097ac7084fc3c881b766
saminyead <Saminyead@gmail.com>
2024-03-25 12:05:21 +0600

    modified tests to test the class method instead of the old functions

--- datatype/tests/test_inference.py
+++ datatype/tests/test_inference.py
@@ -3,42 +3,43 @@ import pandas as pd
 import numpy
 
 from datatype.utils.inference import (
-    infer_numeric, infer_datetime
+    infer_numeric, infer_datetime, DataFrameToInfer
 )
 from datatype.utils.infer_all import infer_all
 
 SAMPLE_DATA_PATH = "sample_data.csv"
 
 @pytest.fixture
-def df_test(filename:str=SAMPLE_DATA_PATH) -> pd.DataFrame:
+def df_to_infer_test(filename:str=SAMPLE_DATA_PATH) -> DataFrameToInfer:
     """Test Dataframe fixture"""
-    return pd.read_csv(filename)
+    df = pd.read_csv(filename)
+    return DataFrameToInfer(df)
 
 
-def test_infer_numeric_80_pct_col_is_numeric(df_test:pd.DataFrame) -> None:
+def test_infer_numeric_80_pct_col_is_numeric(df_to_infer_test:DataFrameToInfer) -> None:
     """Test checks if infer_numeric categorizes columns with more than 80%
     numeric values as numeric"""
-    df_test_inferred: pd.DataFrame = infer_numeric(df_test)
+    df_test_inferred: DataFrameToInfer = df_to_infer_test.infer_numeric()
     assert df_test_inferred['Score'].dtype == numpy.dtype('float')
 
 
 
 def test_infer_datetime_varied_dt_formats(
-        df_test:pd.DataFrame
+        df_to_infer_test:DataFrameToInfer
 ) -> None:
     """Test checks if infer_datetime categorizes columns containing a variety
     of datetime formats"""
-    df_test_inferred: pd.DataFrame = infer_datetime(df_test)
+    df_test_inferred: DataFrameToInfer = df_to_infer_test.infer_datetime()
     assert df_test_inferred['Birthdate'].dtype == 'datetime64[ns]'
     assert df_test_inferred['Test_None_Datetime'].dtype == 'datetime64[ns]'
 
 
 def test_infer_datetime_no_change_to_non_dt_formats(
-        df_test:pd.DataFrame
+        df_to_infer_test:DataFrameToInfer
 ) -> None:
     """infer_datetime should not change other formats (especially integer 
     column that are still string/objects) to datetime formats"""
-    df_test_inferred: pd.DataFrame = infer_datetime(df_test)
+    df_test_inferred: DataFrameToInfer = df_to_infer_test.infer_datetime()
     assert df_test_inferred['Test_None_Numeric'].dtype != 'datetime64[ns]'
     assert df_test_inferred['Test_All_Int'].dtype != 'datetime64[ns]'
     assert df_test_inferred['Score'].dtype != 'datetime64[ns]'
