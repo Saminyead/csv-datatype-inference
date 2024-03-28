a63f66d69f95d3066bb6c010b618483be6dcaa16
saminyead <Saminyead@gmail.com>
2024-03-28 17:23:11 +0600

    added new fields to contain the data as json

--- datatype/models.py
+++ datatype/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class UploadedCSVFile(models.Model):
     file = models.FileField()
     uploaded_on = models.DateTimeField(auto_now_add=True)
+    original_data_json = models.JSONField(null=True,blank=True)
     original_data_types = models.JSONField(null=True,blank=True)
     inferred_data_types = models.JSONField(null=True,blank=True)
+    inferred_data_json = models.JSONField(null=True,blank=True)
 
 
     def arrange_for_response(self):
