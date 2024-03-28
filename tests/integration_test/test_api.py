b8321e61d18920dff814e3a4e307e3832c0146c5
saminyead <Saminyead@gmail.com>
2024-03-29 00:04:08 +0600

    added absolute path for testing from project root new test for checking dataframe as json

--- tests/integration_test/test_api.py
+++ tests/integration_test/test_api.py
@@ -1,10 +1,14 @@
 import pytest
 from rest_framework.test import APIClient
 from rest_framework.response import Response
+import pandas as pd
+import pathlib
 
 
-INVALID_FILE:str = './files/invalid.pdf'
-DATA_FILE:str = './files/data.csv'
+INVALID_FILE: pathlib.Path = pathlib.Path(__file__).resolve().parent\
+    /'files'/'invalid.pdf'
+DATA_FILE:pathlib.Path = pathlib.Path(__file__).resolve().parent\
+    /'files'/'data.csv'
 
 
 @pytest.mark.django_db
@@ -24,7 +28,7 @@ def test_api_invalid_file_format(
 
 
 @pytest.mark.django_db
-def test_api_data_file_format(
+def test_api_data_inferred_types(
     api_client:APIClient,
     api_endpoint: str,
     filename:str = DATA_FILE
@@ -46,6 +50,23 @@ def test_api_data_file_format(
     assert response.status_code == 201
     
     for value in expected_values:
-        assert value in response.data['inferred_data_types']
+        assert value in response.data['inferred_data']['inferred_data_types']
 
-    
+
+@pytest.mark.django_db
+def test_response_dataframes(
+    api_client:APIClient,
+    api_endpoint:str,
+    filename:str = DATA_FILE
+) -> None:
+    response:Response = api_client.post(
+        path=api_endpoint,
+        data={'file':open(filename)},
+        format='multipart'
+    )
+
+    expected_original_df = pd.read_csv(DATA_FILE)
+    expected_original_df_json = expected_original_df.to_json(orient='records')
+
+
+    assert expected_original_df_json == response.data['original_data']['original_data_json']
