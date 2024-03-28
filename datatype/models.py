c3298dea646ac95680b27ff2adc13fe71908b1f0
saminyead <Saminyead@gmail.com>
2024-03-28 21:22:43 +0600

    added field for readable data types

--- datatype/models.py
+++ datatype/models.py
@@ -6,8 +6,10 @@ class UploadedCSVFile(models.Model):
     uploaded_on = models.DateTimeField(auto_now_add=True)
     original_data_json = models.JSONField(null=True,blank=True)
     original_data_types = models.JSONField(null=True,blank=True)
-    inferred_data_types = models.JSONField(null=True,blank=True)
+    original_data_types_readable = models.JSONField(null=True,blank=True)
     inferred_data_json = models.JSONField(null=True,blank=True)
+    inferred_data_types = models.JSONField(null=True,blank=True)
+    inferred_data_types_readable = models.JSONField(null=True,blank=True)
 
 
     def arrange_for_response(self):
@@ -18,10 +20,12 @@ class UploadedCSVFile(models.Model):
             },
             'original_data': {
                 'original_data_json': self.original_data_json,
-                'original_data_types': self.original_data_types
+                'original_data_types': self.original_data_types,
+                'original_data_types_readable': self.original_data_types_readable
             },
             'inferred_data': {
                 'inferred_data_json': self.inferred_data_json,
-                'inferred_data_types':self.inferred_data_types
+                'inferred_data_types':self.inferred_data_types,
+                'inferred_data_types_readable':self.inferred_data_types_readable,
             }
         }
